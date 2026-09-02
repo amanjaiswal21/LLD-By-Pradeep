472e1c2fda59a93d2f8aefc70fc1b63062c29aca
amanjaiswal <amanj2003e@gmail.com>
2026-09-02 20:55:41 +0530

    completed book my show lld question

--- src/main/java/org/example/LLDQuestion/BookMyShow/BookMyShowDemo.java
+++ src/main/java/org/example/LLDQuestion/BookMyShow/BookMyShowDemo.java
@@ -29,79 +29,210 @@ Show
 
  */
 
+import java.awt.print.Book;
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.locks.ReentrantLock;
 
-enum SeatStatus{
-    BOOKED,AVAILABLE,HOLD
+enum SeatStatus {
+    BOOKED, AVAILABLE, HOLD
+}
+
+enum PaymetStatus {
+    SUCCESS, FAILED, IN_PROGRESS
 }
 
 
-class User{
+class User {
     int userId;
 
 }
-enum City{
-    LUCKNOW,DELHI,BANGALORE
+
+enum City {
+    LUCKNOW, DELHI, BANGALORE
 }
 
-class Movie{
+enum SeatCategory {
+    PREMIUM, VIP
+}
+
+
+class Movie {
     String movieName;
     Duration movieTime;
 }
 
-class Show{
+class Show {
     int showId;
     Movie movie;
-    List<ShowSeat>seats;
-    LocalDateTime showTime;
+    LocalDateTime startTime;
+    Map<Integer, SeatStatus> seatStatusMap;
+    Map<Integer, ReentrantLock> seatLocks;
 
+    public boolean lockSeat(List<Integer> seats) {
+        List<Integer> sorted = new ArrayList<>(seats);
+        Collections.sort(sorted);
+        List<ReentrantLock> acquiredLocks = new ArrayList<>();
+
+        //accqire the lock
+        try {
+            for (int seatId : sorted) {
+                ReentrantLock lock = seatLocks.get(seatId);
+                lock.lock();
+                acquiredLocks.add(lock);
+            }
+
+            // check seat status
+
+            for (int seatId : sorted) {
+                SeatStatus status = seatStatusMap.get(seatId);
+                if (status != SeatStatus.AVAILABLE)
+                    return false;
+            }
+            for (int seatId : sorted) {
+                seatStatusMap.put(seatId, SeatStatus.HOLD);
+            }
+            return true;
+        } finally {
+            for (ReentrantLock lock : acquiredLocks) {
+                lock.unlock();
+            }
+        }
+    }
+
+    public void confirmSeats(List<Integer> seatIds) {
+        for (int seatId : seatIds) {
+            seatStatusMap.put(seatId, SeatStatus.BOOKED);
+        }
+    }
+
+    public void releaseSeats(List<Integer> seatIds) {
+        for (int seatId : seatIds) {
+            seatStatusMap.put(seatId, SeatStatus.AVAILABLE);
+        }
+    }
 }
 
-class ShowSeat{
+
+class Seat {
     int seatId;
-    SeatStatus status;
-    double price;
+    SeatCategory seatCategory;
 }
 
-class Screen{
+class Screen {
     int screenId;
-    List<Show>shows;
+    List<Seat> seats;
+    Map<LocalDate, List<Show>> showsByDate;
 }
 
 
-class Theatre{
+class Theatre {
     int theatreId;
     City city;
-    List<Screen>screens;
+    List<Screen> screens;
 
 }
 
-class BookingSystem{
-    Map<City,Theatre>cityTheatreMap;
-    List<Theatre>theatres;
+class TheatreService {
+    public Map<City, List<Theatre>> cityTheatreMap;
+
+    List<Theatre> getTheatreByCity(City city) {
+        return cityTheatreMap.get(city);
+    }
+
+    List<Show> getShowsByDate(Theatre theatre, LocalDate showDate) {
+        List<Show> shows = new ArrayList<>();
+        for (Screen screen : theatre.screens) {
+            for (Show show : screen.showsByDate.get(showDate)) {
+                shows.add(show);
+            }
+        }
+        if (shows.isEmpty()) {
+            throw new RuntimeException("No shows is available at selected date");
+        }
+        return shows;
+    }
 
 }
 
-class Ticket{
+class TheatreController {
 
+    public TheatreService theatreService;
+
+    public TheatreController(TheatreService theatreService) {
+        this.theatreService = theatreService;
+    }
+
+    List<Theatre> getTheatre(City city) {
+        return theatreService.getTheatreByCity(city);
+    }
+
+    List<Show> getAllShowsByDate(Theatre theatre, LocalDate showDate) {
+        return theatreService.getShowsByDate(theatre, showDate);
+
+    }
 }
+    class Booking {
+        public UUID id;
+        public User user;
+        public Show show;
+        public List<Integer> seats;
+        public Payment payment;
 
-class TheatreController{
-    List<Theatre> getTheatre(City city){
-        return null;
+        public Booking(User user, Show show, List seats, Payment payment){
+            this.user=user;
+            this.show=show;
+            this.seats=seats;
+            this.payment=payment;
+        }
     }
 
-    List<Show> getAllShows(Theatre theatre){
-        return null;
+    class Payment {
+        public UUID payementId;
+        public PaymetStatus paymetStatus;
+
+        public Payment(PaymetStatus status){
+            payementId=new UUID(11,22);
+            this.paymetStatus=status;
+        }
     }
 
-    boolean selectSeats(List<ShowSeat>seats,int showId, int userId){
-        return false;
+    class BookingService {
+      Map<UUID,Booking>bookings;
+
+      public Booking createBooking(User user, List<Integer>seats,Show show){
+          if (!show.lockSeat(seats)) {
+              throw new RuntimeException("Seat unavailable");
+          }
+          Payment payment= new Payment(PaymetStatus.SUCCESS);
+
+          if(payment.paymetStatus==PaymetStatus.SUCCESS){
+              show.confirmSeats(seats);
+              Booking booking =  new Booking(user, show, seats, payment);
+              bookings.put(booking.id, booking);
+              return booking;
+          }
+          else{
+              show.releaseSeats(seats);
+              throw new RuntimeException("Payment failed");
+          }
+
+      }
     }
 
+
+class BookingController {
+    public BookingService bookingService;
+
+    public BookingController(BookingService bookingService){
+        this.bookingService=bookingService;
+    }
+
+    public Booking createBooking(User user, List<Integer>seats,Show show){
+       return bookingService.createBooking(user,seats,show);
+    }
 
 }
 
