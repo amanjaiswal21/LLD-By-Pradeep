b0bf324f6ed8cef04248c43986e42ebac9b65777
amanjaiswal <amanj2003e@gmail.com>
2026-09-16 09:29:53 +0530

    implemented adapter and state  design pattern,

--- src/main/java/org/example/designPattern/Structural/Adapter/AdapterDesignPatternDemo.java
+++ src/main/java/org/example/designPattern/Structural/Adapter/AdapterDesignPatternDemo.java
@@ -13,24 +13,24 @@ break.
 To solve this, we use the Adapter Design Pattern.
  */
 
-interface IPaymentProcessor{
+interface IPaymentProcessor {
     void pay();
 }
 
-class Application implements IPaymentProcessor{
+class Application implements IPaymentProcessor {
     @Override
     public void pay() {
         System.out.println("Legacy code");
     }
 }
 
-class StripePayment{
-    void makePayment(){
+class StripePayment {
+    void makePayment() {
         System.out.println("Stripe is making payment");
     }
 }
 
-class StripeAdapter implements IPaymentProcessor{
+class StripeAdapter implements IPaymentProcessor {
     StripePayment stripePayment;
 
     @Override
@@ -46,13 +46,13 @@ In the future, if we want to support another payment gateway such as Paytm, we c
 This way, the client-facing contract remains unchanged, while our system can support multiple payment gateways without breaking existing
 clients. */
 
-class PaytmPayment{
-    void makePayment(){
+class PaytmPayment {
+    void makePayment() {
         System.out.println("Paytm is making payment");
     }
 }
 
-class PaytmAdapter implements IPaymentProcessor{
+class PaytmAdapter implements IPaymentProcessor {
     PaytmPayment paytmPayment;
 
     @Override
